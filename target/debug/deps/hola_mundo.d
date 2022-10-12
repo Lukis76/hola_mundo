fba9d66f1e22ee30f91765b0aaddf743e731024b
lukis76 <kiu.manoguerra@gmail.com>
2022-10-12 00:25:07 -0300

    print

---
+++ target/debug/deps/hola_mundo.d
@@ -0,0 +1,5 @@
+C:\Users\kiuma\Documents\Rust\aprendiendoRust\hola_mundo\target\debug\deps\hola_mundo.exe: src\main.rs
+
+C:\Users\kiuma\Documents\Rust\aprendiendoRust\hola_mundo\target\debug\deps\hola_mundo.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/hola_mundo.d
@@ -0,0 +1,1 @@
+C:\Users\kiuma\Documents\Rust\aprendiendoRust\hola_mundo\target\debug\hola_mundo.exe: C:\Users\kiuma\Documents\Rust\aprendiendoRust\hola_mundo\src\main.rs
